f012f0776a3c3053b87c6658bad2915fe5ead07a
Yukai Zou <yukaizou2012@gmail.com>
2015-07-25 01:22:06 -0700

    Format issues during import/export fixed, codes for making new matrixes drafted

--- designmat.R
+++ designmat.R
@@ -1,6 +1,10 @@
 designmat <- function(x) {
-        data <- read.csv("Hello.mat")
-        write.table(data, paste(x,".mat", sep = ""), quote = FALSE, row.names = FALSE)
-        # Formatting of the first line "X.NumWaves.3" should be changed to "/NumWaves 3"
+        data <- read.csv("Hello.mat", stringsAsFactors = FALSE)
+        write.table(data, paste(x,".mat", sep = ""), quote = FALSE, row.names = FALSE, col.names = FALSE)
         # Modify data to add in covariate
+        data$V1[4] <- paste(1,0,100)
+        # Current solution: extract the first 4 rows from original data, convert to matrix by cbind,
+        # create a covariate matrix containing characters (also using cbind), rbind the two matrix,
+        # convert to data frame, and then write into a file.
+        # The 4 rows need to be updated since the new covariate matrix will contain different numbers.
 }
