42d1026156c7ecac166032d739ade451cd709071
YukaiZou <yukaizou2012@gmail.com>
2015-07-26 00:16:29 -0700

    New covariate column added, still need to configure the updated text information instead of borrowing the old texts. The example R file was a inspire from Stackflow which leads to the updated codes today.

--- designmat.R
+++ designmat.R
@@ -1,10 +1,14 @@
 designmat <- function(x) {
-        data <- read.csv("Hello.mat", stringsAsFactors = FALSE)
-        write.table(data, paste(x,".mat", sep = ""), quote = FALSE, row.names = FALSE, col.names = FALSE)
-        # Modify data to add in covariate
-        data$V1[4] <- paste(1,0,100)
-        # Current solution: extract the first 4 rows from original data, convert to matrix by cbind,
-        # create a covariate matrix containing characters (also using cbind), rbind the two matrix,
-        # convert to data frame, and then write into a file.
-        # The 4 rows need to be updated since the new covariate matrix will contain different numbers.
+        data <- read.csv("Hello.mat", header = FALSE, stringsAsFactors = FALSE)
+        # Codes for update:
+                ori <- data$V1[5:length(data$V1)] # Extract the original matrix data
+                cov <- as.character(c(1:10)) # Obtain the covariate data (This is just an example), need update based on the specific data
+                data2 <- data.frame(cbind(ori, cov)) # Create a matrix by column-combining the original matrix data and the covariate data, and convert to dataframe
+                cols <- names(data2) # Define the columns to be pasted
+                data2$x <- apply( data2[ , cols] , 1, paste , collapse = " " ) # Combine the two components through paste function
+                data3 <- data2[,!(names(data2) %in% cols)] # Update the dataframe with the previous two components excluded
+                data4 <- c(data$V1[1:4], data3) # Create a character composed of strings of texts and data matrix
+                        # Need to configure and update the text information, instead of using the old text information
+        # Export data:        
+        write.table(data4, paste(x,".mat", sep = ""), quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
