c8870baac313c5f39dba5fdb7b9160312d08fadc
Yukai Zou <yukaizou2012@gmail.com>
2015-07-26 11:25:59 -0700

    Brief description update

--- designmat.R
+++ designmat.R
@@ -1,4 +1,12 @@
 designmat <- function(filename = "design", Covdata = "Covariate.xlsx", demean = TRUE) {
+## This is a function written to generate design.mat file automatically
+## for TBSS analysis. A successful execution of this function requires
+## 1) Using "design_ttest2 design" to first create a design.mat;
+## 2) Create a "Covariate.xlsx" file and put in the same directory.
+## The "demean" call is not very specific, as some covariate might not
+## need to be demeaned, so this will be updated in the future.
+## designcov.R will come soon.
+
         # Read in the data
         data <- read.csv("design.mat", header = FALSE, stringsAsFactors = FALSE)
         # Read in the covariate sheet
@@ -12,8 +20,7 @@ designmat <- function(filename = "design", Covdata = "Covariate.xlsx", demean = 
                         }
                         Covdata <- demean()
                 }
-        # Create new heading
-        # Configure NW, NP, PP
+        # Create new heading with NW, NP, PP configured. Normally the default do not need to be changed.
         makehead <- function(NW = 2 + ncol(Covdata), NP = nrow(Covdata), PP = "1 1") { # A function creating the heading
                 NumWaves <- paste("/NumWaves", NW)
                 NumPoints <- paste("/NumPoints", NP)
