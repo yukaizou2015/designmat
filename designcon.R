e822dd9f94d33640dc7c7552d62a9867a724d455
Yukai Zou <yukaizou2012@gmail.com>
2015-07-26 14:21:46 -0700

    Stop warning added, able to configure text heading and combine with the updated matrix

---
+++ designcon.R
@@ -0,0 +1,34 @@
+designcon <- function(filename = "designtest", NC = 3) {
+        # Read in the data
+        data <- read.csv("design.con", header = FALSE, stringsAsFactors = FALSE)
+        data2 <- data$V1[5:length(data$V1)]
+        data2 <- data.frame(cbind(data2), stringsAsFactors = FALSE)
+        
+        if (NC < 3) {
+                stop("Invalid Number of Contrasts, should be not less than 3")
+        } else {
+                for (i in 1:(NC-2)) {
+                        ncols <- length(strsplit(data2$data2[1], " ")[[1]]) # Configure columns number
+                        ori <- data2$data2[1:length(data2$data2)]
+                        EV1 <- c(ori, rep(paste(rep("0", ncols), collapse = " "), 2))
+                        EV2 <- c(rep("0", length(data2$data2)), 1, -1)
+                        data2 <- data.frame(cbind(EV1,EV2))
+                        cols <- names(data2)
+                        data2$x <- apply(data2, 1, paste, collapse = " ")
+                        data2 <- data2[,!(names(data2) %in% cols)]
+                        data2 <- data.frame(data2, stringsAsFactors = FALSE)
+                }
+                # Need to configure headings
+                # Configure NW, NC, PP
+                NumWaves <- paste("/NumWaves", NC)
+                NumContrasts <- paste("/NumContrasts", NC)
+                PPheights <- paste("/PPheights", PP)
+                Matrix <- "/Matrix"
+                heading <- c(NumWaves, NumContrasts, PPheights, Matrix)
+                # Combine heading and updated matrix
+                data3 <- cbind(c(heading, data2$data2))
+                data3 <- data.frame(data3)
+                # Export data
+                write.table(data3, paste(filename,".con", sep = ""), quote = FALSE, row.names = FALSE, col.names = FALSE)
+        }
+}
